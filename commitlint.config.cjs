00dacc8cfdfc845587c189bd154250758061dce8
huytv04883 <huytv04883@gmail.com>
2025-12-24 14:35:11 +0700

    add husky and lint-staged

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'perf', 'style', 'docs', 'test', 'chore'],
+    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case']],
+    'subject-max-length': [2, 'always', 72],
+    'scope-empty': [1, 'never'],
+  },
+};
